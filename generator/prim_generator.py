021bfa04ef0667399da7af8ff80b966f28cbae61
mark4653 <40498369+mark4653@users.noreply.github.com>
2024-12-03 00:14:14 +0900

    알고리즘 주석 추가

--- generator/prim_generator.py
+++ generator/prim_generator.py
@@ -7,24 +7,30 @@ def generator(grid_size):
     visited = []
     s = set()
     x, y = (0, 0)
+    # 맨 처음 좌표부터 시작
     grid[x][y] = 1
     visited.append((x, y, grid[x][y]))
+    #x, y의 프론티어를 계산 후 집합 s에 추가
     fs = frontier(x, y, grid, grid_size)
     for f in fs:
         s.add(f)
+    #s가 비어있지 않다면 임의의 셀을 제거
     while s:
         x, y = random.choice(tuple(s))
         s.remove((x, y))
+        #x, y의 이웃 계산
         ns = neighbours(x, y, grid, grid_size)
         if ns:
             nx, ny = random.choice(tuple(ns))
+            #랜덤한 이웃중 하나랑 연결
             connect(x, y, nx, ny, grid, visited)
         fs = frontier(x, y, grid, grid_size)
+        #(x, y)의 프론티어를 s에 추가
         for f in fs:
             s.add(f)
             
     return visited
-            
+#대각선을 제외한 벽을 계산            
 def frontier(x, y, grid, grid_size):
     f = set()
     if x >= 0 and x < grid_size and y >= 0 and y < grid_size:
@@ -37,7 +43,7 @@ def frontier(x, y, grid, grid_size):
         if y + 2 < grid_size and not grid[x][y+2]:
              f.add((x, y+2))    
     return f
-    
+#대각선을 제외한 통로를 계산       
 def neighbours(x, y, grid, grid_size):
     n = set()
     if x >= 0 and x < grid_size and y >= 0 and y < grid_size:
